a2d016d4c7d4bc3c2b65f0fc87c76dcb84239db6
NANO <File changed>
2022-07-03 14:53:22 +0600

    Third submission

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -3,11 +3,20 @@ package lang.print.gaps.task3;
 public class ReassigningValues {
     public static void main(String[] args) {
         int first = 1;
-        System.out.println("first");
+        System.out.println(first);
         int second = 10;
-        System.out.println("second");
+        System.out.println(second);
         int third = 100;
-        System.out.println("third");
+        System.out.println(third);
+        int linkToFirst;
+        linkToFirst = first = 15;
+        System.out.println( linkToFirst);
+        int linkToSecond;
+        linkToSecond = second = 6;
+        System.out.println( linkToSecond);
+        int linkToThird;
+        linkToThird = third = 4;
+        System.out.println( linkToThird);
 
     }
 }
